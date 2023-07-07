3f6b63999fae4d77dad783b0a45f6215a7b919c6
tanimsk <sktanim5800@gmail.com>
2023-07-07 17:24:38 +0600

    projects

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,3 +1,36 @@
+import styles from "./Projects.module.css";
+import rterminal from '../../assets/rterminal.png';
+import p2a from '../../assets/p2a.png';
+
+const Cards = (props) => {
+    return (
+        <div className={styles.card}>
+            <a href={props.link} target='_blank'>
+                <img src={props.img_src} alt="Certificate" />
+            </a>
+        </div>
+    );
+}
+
+
 export default function Projects() {
-    return (<></>);
+    const imgLink = [
+        rterminal,
+        p2a
+    ];
+
+    const links = [
+        "https://github.com/TanimSk/Remote-Terminal",
+        "https://github.com/TanimSk/poseToAnimation"
+    ];
+
+    return (
+        <div className={styles.container}>
+            {
+                imgLink.map((src, index) =>
+                    <Cards img_src={src} link={links[index]} key={index}/>
+                )
+            }
+        </div>
+    );
 }
