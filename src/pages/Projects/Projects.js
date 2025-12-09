ce611ba1eef5a030f923034e4372035e1a713791
Tanim Sk <sktanim5800@gmail.com>
2025-12-10 01:08:36 +0600

    Update Projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -2,6 +2,7 @@ import styles from "./Projects.module.css";
 import rterminal from '../../assets/rterminal.png';
 import p2a from '../../assets/p2a.png';
 import djr from '../../assets/dj_react.png'
+import wroFE from '../../assets/FE-vehicle.png'
 
 const Cards = (props) => {
     return (
@@ -20,12 +21,14 @@ const Cards = (props) => {
 export default function Projects() {
     const imgLink = [
         rterminal,
+        wroFE,
         p2a,
         djr
     ];
 
     const links = [
         "https://github.com/TanimSk/Remote-Terminal",
+        "https://github.com/TanimSk/WRO-FE-2025_Team-Echo-Drift",
         "https://github.com/TanimSk/poseToAnimation",
         "https://github.com/TanimSk/django-react-ecommerce/tree/django_drf"
     ];
